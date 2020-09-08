b156116249ee4fda2b197ea65657693e670488e4
Chawalitr <info@bkk.ac>
2020-09-08 23:14:09 +0700

    finish homepage for child

--- elements/user_left_sidebar.php
+++ elements/user_left_sidebar.php
@@ -1,17 +1,16 @@
-<?php
-?>
-<aside>
+
+<aside class="left-side">
     <div class="display-profile">
-        <a href="#"><img src="" alt=""/></a>
+        <a href="#"><img src="/images/example/kid-thinking-left.png" alt=""/></a>
     </div>
     <nav>
-        <a href="#" class="explore"><i></i><span>Explore</span></a>
-        <a href="#" class="save-post"><i></i><span>Save Post</span></a>
-        <a href="#" class="my-groups"><i></i><span>My Groups</span></a>
-        <a href="#" class="my-pages"><i></i><span>My Pages</span></a>
-        <a href="#" class="my-albums"><i></i><span>My Albums</span></a>
+        <a href="#"><i class="explore"></i><span>Explore</span></a>
+        <a href="#"><i class="save-post"></i><span>Save Post</span></a>
+        <a href="#"><i class="my-groups"></i><span>My Groups</span></a>
+        <a href="#"><i class="my-pages"></i><span>My Pages</span></a>
+        <a href="#"><i class="my-albums"></i><span>My Albums</span></a>
         <hr />
-        <a href="#" class="games"><i></i><span>Games</span></a>
-        <a href="#" class="events"><i></i><span>Events</span></a>
+        <a href="#"><i class="games"></i><span>Games</span></a>
+        <a href="#"><i class="events"></i><span>Events</span></a>
     </nav>
 </aside>
